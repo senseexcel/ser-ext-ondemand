1f9d9c21f81246896cdc2e1150f19f669495c5a4
Thomas Hänig <thomas.haenig@akquinet.de>
2018-05-18 15:55:15 +0200

    update build process

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,33 +1,59 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const PathOverridePlugin = require('path-override-webpack-plugin');
-const ZipPlugin = require('zip-webpack-plugin');
-const PKG = require('./package.json');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const PathOverridePlugin = require("path-override-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
+const PKG = require("./package.json");
 const packagenName = PKG.name;
-const deployPath = 'dist';
-const WebPackDeployAfterBuild = require('webpack-deploy-after-build');
+const deployPath = "dist";
+const WebPackDeployAfterBuild = require("webpack-deploy-after-build");
 
 function CssLoaderReplacerPlugin(options) {
 }
 
-CssLoaderReplacerPlugin.prototype.apply = function(resolver) {
-    resolver.plugin("normal-module-factory", function(nmf) {
-        nmf.plugin("before-resolve", function(result, callback) {
+function checkForCssInRoot(result) {
+    if(result.request.indexOf("node_modules") === -1 && 
+       result.request.indexOf("css!") > -1 &&
+       result.context.indexOf("node_modules") === -1) {
+        return true;
+    }
+    return false;
+}
 
-            if(result.request.indexOf("node_modules") === -1 && 
-               result.request.indexOf("css!") > -1 &&
-               result.context.indexOf("node_modules") === -1) {
-                result.request = result.request.replace("css!./", "./");
-                result.request = result.request.replace(".css", ".less");
-            }
-            return callback();
-        });
-    });
+function checkForCssInDavinci(result) {
+    if(result.request.indexOf("node_modules") === -1 && 
+       result.request.indexOf("css!") > -1 &&
+       result.context.indexOf("node_modules\\davinci.js") > -1) {
+        return true;
+    }
+    return false;
+}
+
+function replaceLess(result, callback) {
+
+    if(checkForCssInRoot(result)) {
+
+        result.request = result.request.replace("css!./", "./");
+        result.request = result.request.replace(".css", ".less");
+    }
+
+    if(checkForCssInDavinci(result)) {
+
+        result.request = result.request.replace("css!./", "./");
+    }
+    return callback();
+}
+
+function cbLoadNMFPlugin(nmf) {
+    nmf.plugin("before-resolve", replaceLess);
+}
+
+CssLoaderReplacerPlugin.prototype.apply = function(resolver) {
+    resolver.plugin("normal-module-factory", cbLoadNMFPlugin);
 };
 
 let config = {
-    mode: 'production',
+    mode: "production",
     context: path.join(__dirname),
     optimization: {
         minimize: false
@@ -40,12 +66,12 @@ let config = {
     },
     module: {
         rules: [
-            { test: /text!.*\.html$/, use: 'raw-loader'},
+            { test: /text!.*\.html$/, use: "raw-loader"},
             { test: /\.tsx?$/, 
-                loader: 'ts-loader',
+                loader: "ts-loader",
                 exclude: [/node_modules/],
                 options: {
-                    configFile: 'tsconfigBuild.json'
+                    configFile: "tsconfigBuild.json"
                 }
             },
             { test: /\.less$/, use: [{
@@ -56,68 +82,71 @@ let config = {
                     loader: "less-loader"
                 }]
             },
-            { test: /css!.*\.css$/, use:  [
+            { test: /(\.css$)/, use:  [
                 {
-                    loader: 'style-loader',
+                    loader: "style-loader",
                     options: {
                         convertToAbsoluteUrls: true
                     }
                 },
                 {
-                    loader: 'css-loader',
-                    options: { modules: false, minimize: true, importLoaders: 1 },
+                    loader: "css-loader",
+                    options: { 
+                        modules: false,
+                        importLoaders: 1
+                    },
                 },
                 {
-                    loader: 'postcss-loader',
+                    loader: "postcss-loader",
                     options: {
                         plugins: () => ([
-                        require('autoprefixer'),
-                        require('precss'),
+                        require("autoprefixer"),
+                        require("precss"),
                         ]),
                     },
                 },
             ]},
-            { test: /\.eot(\?\S*)?$/, loader: 'url-loader?limit=100000&mimetype=application/vnd.ms-fontobject' },
-            { test: /\.woff2(\?\S*)?$/, loader: 'url-loader?limit=100000&mimetype=application/font-woff2' },
-            { test: /\.woff(\?\S*)?$/, loader: 'url-loader?limit=100000&mimetype=application/font-woff' },
-            { test: /\.ttf(\?\S*)?$/, loader: 'url-loader?limit=100000&mimetype=application/font-ttf' },
-            { test: /\.svg(\?\S*)?$/, loader: 'url-loader?limit=100000&mimetype=image/svg+xml'},
-            { test: /\.(png|jpg|gif)$/, loader: 'url-loader', options: { limit: 10000 } }
+            { test: /\.eot$/, loader: "url-loader?limit=100000&mimetype=application/vnd.ms-fontobject" },
+            { test: /\.woff2$/, loader: "url-loader?limit=100000&mimetype=application/font-woff2" },
+            { test: /\.woff$/, loader: "url-loader?limit=100000&mimetype=application/font-woff" },
+            { test: /\.ttf$/, loader: "url-loader?limit=100000&mimetype=application/font-ttf" },
+            { test: /\.svg$/, loader: "url-loader?limit=100000&mimetype=image/svg+xml"},
+            { test: /\.(png|jpg|gif)$/, loader: "url-loader", options: { limit: 10000 } }
         ]
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: [".ts", ".tsx", ".js"]
     },
     resolveLoader: {
         alias: {
-            'text': 'raw-loader',
-            'css': 'css-loader'
+            "text": "raw-loader",
+            "css": "css-loader"
         }
     },
     externals: [
-        { angular: 'angular' },
-        { qvangular: 'qvangular' },
-        { qlik: 'qlik' }
+        { angular: "angular" },
+        { qvangular: "qvangular" },
+        { qlik: "qlik" }
     ],
     plugins: [
         new CssLoaderReplacerPlugin({options: true}),
         new CleanWebpackPlugin( (deployPath), { allowExternal: true } ),
         new CleanWebpackPlugin( ("placeholder"), { allowExternal: true } ),
-        new PathOverridePlugin(/\/umd\//, '/esm/'),
+        new PathOverridePlugin(/\/umd\//, "/esm/"),
         new CopyWebpackPlugin([
             { from: `${packagenName}.qext`, to: `${packagenName}.qext`},
-            { from: `wbfolder.wbl`, to: `wbfolder.wbl`}
+            { from: "wbfolder.wbl", to: "wbfolder.wbl"}
         ]),
         new ZipPlugin({
-            path: './',
+            path: "./",
             filename: `${packagenName}.zip`,
         }),
         new WebPackDeployAfterBuild({
-            from: path.resolve(__dirname, './dist'),
-            to: 'C:/QlikShare/StaticContent/Extensions/ser-ext-ondemand/'
+            from: path.resolve(__dirname, "./dist"),
+            to: "C:/QlikShare/StaticContent/Extensions/q2g-ext-bookmark/"
         })
     ]
-}
+};
 
 
 config.plugins = [
@@ -128,5 +157,4 @@ config.plugins = [
  * Export for npm
  */
 module.exports = config;
-
 
